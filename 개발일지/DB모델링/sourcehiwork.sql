bd2d7f6aa0fe8cb3c1dc7765949fe00158af3d7d
KimYuah <www0157@naver.com>
2020-11-18 15:52:55 +0900

    소스파일 공유

---
+++ 개발일지/DB모델링/sourcehiwork.sql
@@ -0,0 +1,486 @@
+-- 게시글 정보
+DROP TABLE IF EXISTS cms_bList RESTRICT;
+
+-- 회사정보
+DROP TABLE IF EXISTS cms_cInfo RESTRICT;
+
+-- 팀 목록
+DROP TABLE IF EXISTS cms_tList RESTRICT;
+
+-- 사원
+DROP TABLE IF EXISTS cms_worker RESTRICT;
+
+-- 권한
+DROP TABLE IF EXISTS cms_auth RESTRICT;
+
+-- 휴가 신청
+DROP TABLE IF EXISTS cms_vApp RESTRICT;
+
+-- 휴가 종류 
+DROP TABLE IF EXISTS cms_vKind RESTRICT;
+
+-- 댓글 정보
+DROP TABLE IF EXISTS cms_cmtInfo RESTRICT;
+
+-- 대화방
+DROP TABLE IF EXISTS cms_cRoom RESTRICT;
+
+-- 일정
+DROP TABLE IF EXISTS cms_calendar RESTRICT;
+
+-- 게시판
+DROP TABLE IF EXISTS cms_board RESTRICT;
+
+-- 대화
+DROP TABLE IF EXISTS cms_chat RESTRICT;
+
+-- 대화방참여자
+DROP TABLE IF EXISTS cms_cGroup RESTRICT;
+
+-- 게시글 정보
+CREATE TABLE cms_bList (
+  bNo      INTEGER     NOT NULL COMMENT '게시글번호', -- 게시글번호
+  COL      INTEGER     NOT NULL COMMENT '게시판번호', -- 게시판번호
+  wNo      INTEGER     NOT NULL COMMENT '사원 번호', -- 사원 번호
+  title    VARCHAR(50) NOT NULL COMMENT '제목', -- 제목
+  regiDate DATE        NOT NULL COMMENT '등록일', -- 등록일
+  vCount   INTEGER     NOT NULL COMMENT '조회수', -- 조회수
+  aFile    VARCHAR(50) NULL     COMMENT '첨부파일명', -- 첨부파일명
+  fSize    MEDIUMTEXT  NULL     COMMENT '첨부파일 용량', -- 첨부파일 용량
+  content  MEDIUMTEXT  NULL     COMMENT '글내용' -- 글내용
+)
+COMMENT '게시글 정보';
+
+-- 게시글 정보
+ALTER TABLE cms_bList
+  ADD CONSTRAINT PK_cms_bList -- 게시글 정보 기본키
+    PRIMARY KEY (
+      bNo -- 게시글번호
+    );
+
+ALTER TABLE cms_bList
+  MODIFY COLUMN bNo INTEGER NOT NULL AUTO_INCREMENT COMMENT '게시글번호';
+
+-- 회사정보
+CREATE TABLE cms_cInfo (
+  crNo   INTEGER      NOT NULL COMMENT '법인등록번호', -- 법인등록번호
+  cName  VARCHAR(50)  NOT NULL COMMENT '회사명', -- 회사명
+  map    MEDIUMTEXT   NOT NULL COMMENT '지도', -- 지도
+  postNo INTEGER      NOT NULL COMMENT '우편번호', -- 우편번호
+  addr   VARCHAR(255) NOT NULL COMMENT '주소', -- 주소
+  tel    VARCHAR(30)  NOT NULL COMMENT '전화번호', -- 전화번호
+  fax    VARCHAR(30)  NOT NULL COMMENT '팩스번호' -- 팩스번호
+)
+COMMENT '회사정보';
+
+-- 회사정보
+ALTER TABLE cms_cInfo
+  ADD CONSTRAINT PK_cms_cInfo -- 회사정보 기본키
+    PRIMARY KEY (
+      crNo -- 법인등록번호
+    );
+
+-- 팀 목록
+CREATE TABLE cms_tList (
+  tCode VARCHAR(20) NOT NULL COMMENT '팀 코드', -- 팀 코드
+  crNo  INTEGER     NOT NULL COMMENT '법인등록번호', -- 법인등록번호
+  tName VARCHAR(50) NOT NULL COMMENT '팀 부서' -- 팀 부서
+)
+COMMENT '팀 목록';
+
+-- 팀 목록
+ALTER TABLE cms_tList
+  ADD CONSTRAINT PK_cms_tList -- 팀 목록 기본키
+    PRIMARY KEY (
+      tCode -- 팀 코드
+    );
+
+-- 사원
+CREATE TABLE cms_worker (
+  wNo      INTEGER      NOT NULL COMMENT '사원 번호', -- 사원 번호
+  aCode    INTEGER      NOT NULL COMMENT '권한 코드', -- 권한 코드
+  tCode    VARCHAR(20)  NULL     COMMENT '팀 코드', -- 팀 코드
+  wName    VARCHAR(50)  NOT NULL COMMENT '사원이름', -- 사원이름
+  email    VARCHAR(40)  NOT NULL COMMENT '이메일', -- 이메일
+  wPhoto   VARCHAR(255) NOT NULL COMMENT '사원사진', -- 사원사진
+  rank     MEDIUMTEXT   NOT NULL COMMENT '직급', -- 직급
+  eType    MEDIUMTEXT   NOT NULL COMMENT '고용형태', -- 고용형태
+  regiDate DATE         NOT NULL COMMENT '입사일', -- 입사일
+  tel      INTEGER      NOT NULL COMMENT '휴대폰번호', -- 휴대폰번호
+  gender   INTEGER      NOT NULL COMMENT '성별', -- 성별
+  tAnnu    INTEGER      NOT NULL COMMENT '총 연차', -- 총 연차
+  pwd      INTEGER      NOT NULL COMMENT '비밀번호', -- 비밀번호
+  eSt      INTEGER      NOT NULL COMMENT '재직 상태' -- 재직 상태
+)
+COMMENT '사원';
+
+-- 사원
+ALTER TABLE cms_worker
+  ADD CONSTRAINT PK_cms_worker -- 사원 기본키
+    PRIMARY KEY (
+      wNo -- 사원 번호
+    );
+
+-- 이메일
+CREATE UNIQUE INDEX UIX_cms_worker
+  ON cms_worker ( -- 사원
+    email ASC -- 이메일
+  );
+
+-- 휴대폰번호
+CREATE UNIQUE INDEX UIX_cms_worker2
+  ON cms_worker ( -- 사원
+    tel ASC -- 휴대폰번호
+  );
+
+ALTER TABLE cms_worker
+  MODIFY COLUMN wNo INTEGER NOT NULL AUTO_INCREMENT COMMENT '사원 번호';
+
+-- 권한
+CREATE TABLE cms_auth (
+  aCode INTEGER     NOT NULL COMMENT '권한 코드', -- 권한 코드
+  name  VARCHAR(50) NOT NULL COMMENT '권한명' -- 권한명
+)
+COMMENT '권한';
+
+-- 권한
+ALTER TABLE cms_auth
+  ADD CONSTRAINT PK_cms_auth -- 권한 기본키
+    PRIMARY KEY (
+      aCode -- 권한 코드
+    );
+
+-- 권한 유니크 인덱스
+CREATE UNIQUE INDEX UIX_cms_auth
+  ON cms_auth ( -- 권한
+    name ASC -- 권한명
+  );
+
+-- 휴가 신청
+CREATE TABLE cms_vApp (
+  COL   INTEGER     NOT NULL COMMENT '휴가신청번호', -- 휴가신청번호
+  vCode VARCHAR(20) NOT NULL COMMENT '휴가 코드', -- 휴가 코드
+  wNo   INTEGER     NOT NULL COMMENT '사원 번호', -- 사원 번호
+  adt   DATE        NOT NULL COMMENT '신청일', -- 신청일
+  sdt   DATE        NOT NULL COMMENT '시작일', -- 시작일
+  edt   DATE        NOT NULL COMMENT '종료일', -- 종료일
+  used  INTEGER     NOT NULL COMMENT '사용연차일수', -- 사용연차일수
+  rs    MEDIUMTEXT  NOT NULL COMMENT '신청사유', -- 신청사유
+  appr  MEDIUMTEXT  NOT NULL COMMENT '승인여부' -- 승인여부
+)
+COMMENT '휴가 신청';
+
+-- 휴가 신청
+ALTER TABLE cms_vApp
+  ADD CONSTRAINT PK_cms_vApp -- 휴가 신청 기본키
+    PRIMARY KEY (
+      COL -- 휴가신청번호
+    );
+
+-- 휴가 종류 
+CREATE TABLE cms_vKind (
+  vCode VARCHAR(20) NOT NULL COMMENT '휴가 코드', -- 휴가 코드
+  name  VARCHAR(50) NOT NULL COMMENT '휴가명' -- 휴가명
+)
+COMMENT '휴가 종류 ';
+
+-- 휴가 종류 
+ALTER TABLE cms_vKind
+  ADD CONSTRAINT PK_cms_vKind -- 휴가 종류  기본키
+    PRIMARY KEY (
+      vCode -- 휴가 코드
+    );
+
+-- 댓글 정보
+CREATE TABLE cms_cmtInfo (
+  cCode VARCHAR(20) NOT NULL COMMENT '댓글 코드', -- 댓글 코드
+  bNo   INTEGER     NOT NULL COMMENT '게시글번호', -- 게시글번호
+  COL3  INTEGER     NOT NULL COMMENT '순서', -- 순서
+  COL4  INTEGER     NOT NULL COMMENT '단계', -- 단계
+  COL   MEDIUMTEXT  NOT NULL COMMENT '내용', -- 내용
+  rd    DATE        NOT NULL COMMENT '등록일', -- 등록일
+  wNo   INTEGER     NOT NULL COMMENT '사원 번호' -- 사원 번호
+)
+COMMENT '댓글 정보';
+
+-- 댓글 정보
+ALTER TABLE cms_cmtInfo
+  ADD CONSTRAINT PK_cms_cmtInfo -- 댓글 정보 기본키
+    PRIMARY KEY (
+      cCode -- 댓글 코드
+    );
+
+-- 대화방
+CREATE TABLE cms_cRoom (
+  gChatCode VARCHAR(20) NOT NULL COMMENT '대화방번호', -- 대화방번호
+  cName     VARCHAR(50) NOT NULL COMMENT '채팅방명', -- 채팅방명
+  cExpl     MEDIUMTEXT  NULL     COMMENT '채팅방설명' -- 채팅방설명
+)
+COMMENT '대화방';
+
+-- 대화방
+ALTER TABLE cms_cRoom
+  ADD CONSTRAINT PK_cms_cRoom -- 대화방 기본키
+    PRIMARY KEY (
+      gChatCode -- 대화방번호
+    );
+
+-- 일정
+CREATE TABLE cms_calendar (
+  calCode VARCHAR(20)  NOT NULL COMMENT '일정코드', -- 일정코드
+  wNo     INTEGER      NOT NULL COMMENT '사원 번호', -- 사원 번호
+  title   VARCHAR(50)  NOT NULL COMMENT '제목', -- 제목
+  sdt     DATE         NOT NULL COMMENT '시작일', -- 시작일
+  edt     DATE         NOT NULL COMMENT '종료일', -- 종료일
+  content MEDIUMTEXT   NOT NULL COMMENT '내용', -- 내용
+  place   VARCHAR(255) NULL     COMMENT '장소' -- 장소
+)
+COMMENT '일정';
+
+-- 일정
+ALTER TABLE cms_calendar
+  ADD CONSTRAINT PK_cms_calendar -- 일정 기본키
+    PRIMARY KEY (
+      calCode -- 일정코드
+    );
+
+-- 게시판
+CREATE TABLE cms_board (
+  COL  INTEGER     NOT NULL COMMENT '게시판번호', -- 게시판번호
+  COL2 VARCHAR(50) NOT NULL COMMENT '게시판명' -- 게시판명
+)
+COMMENT '게시판';
+
+-- 게시판
+ALTER TABLE cms_board
+  ADD CONSTRAINT PK_cms_board -- 게시판 기본키
+    PRIMARY KEY (
+      COL -- 게시판번호
+    );
+
+-- 대화
+CREATE TABLE cms_chat (
+  COL       INTEGER     NOT NULL COMMENT '대화번호', -- 대화번호
+  wNo       INTEGER     NOT NULL COMMENT '사원 번호', -- 사원 번호
+  gChatCode VARCHAR(20) NOT NULL COMMENT '대화방번호', -- 대화방번호
+  COL2      MEDIUMTEXT  NOT NULL COMMENT '메시지', -- 메시지
+  COL4      VARCHAR(50) NOT NULL COMMENT '메시지유형', -- 메시지유형
+  COL3      DATE        NOT NULL COMMENT '생성일' -- 생성일
+)
+COMMENT '대화';
+
+-- 대화
+ALTER TABLE cms_chat
+  ADD CONSTRAINT PK_cms_chat -- 대화 기본키
+    PRIMARY KEY (
+      COL -- 대화번호
+    );
+
+-- 대화방참여자
+CREATE TABLE cms_cGroup (
+  wNo       INTEGER     NOT NULL COMMENT '사원 번호', -- 사원 번호
+  gChatCode VARCHAR(20) NOT NULL COMMENT '대화방번호' -- 대화방번호
+)
+COMMENT '대화방참여자';
+
+-- 대화방참여자
+ALTER TABLE cms_cGroup
+  ADD CONSTRAINT PK_cms_cGroup -- 대화방참여자 기본키
+    PRIMARY KEY (
+      wNo,       -- 사원 번호
+      gChatCode  -- 대화방번호
+    );
+
+-- 게시글 정보
+ALTER TABLE cms_bList
+  ADD CONSTRAINT FK_cms_board_TO_cms_bList -- 게시판 -> 게시글 정보
+    FOREIGN KEY (
+      COL -- 게시판번호
+    )
+    REFERENCES cms_board ( -- 게시판
+      COL -- 게시판번호
+    );
+
+-- 게시글 정보
+ALTER TABLE cms_bList
+  ADD CONSTRAINT FK_cms_worker_TO_cms_bList -- 사원 -> 게시글 정보
+    FOREIGN KEY (
+      wNo -- 사원 번호
+    )
+    REFERENCES cms_worker ( -- 사원
+      wNo -- 사원 번호
+    );
+
+-- 팀 목록
+ALTER TABLE cms_tList
+  ADD CONSTRAINT FK_cms_cInfo_TO_cms_tList -- 회사정보 -> 팀 목록
+    FOREIGN KEY (
+      crNo -- 법인등록번호
+    )
+    REFERENCES cms_cInfo ( -- 회사정보
+      crNo -- 법인등록번호
+    );
+
+-- 사원
+ALTER TABLE cms_worker
+  ADD CONSTRAINT FK_cms_auth_TO_cms_worker -- 권한 -> 사원
+    FOREIGN KEY (
+      aCode -- 권한 코드
+    )
+    REFERENCES cms_auth ( -- 권한
+      aCode -- 권한 코드
+    );
+
+-- 사원
+ALTER TABLE cms_worker
+  ADD CONSTRAINT FK_cms_tList_TO_cms_worker -- 팀 목록 -> 사원
+    FOREIGN KEY (
+      tCode -- 팀 코드
+    )
+    REFERENCES cms_tList ( -- 팀 목록
+      tCode -- 팀 코드
+    );
+
+-- 휴가 신청
+ALTER TABLE cms_vApp
+  ADD CONSTRAINT FK_cms_vKind_TO_cms_vApp -- 휴가 종류  -> 휴가 신청
+    FOREIGN KEY (
+      vCode -- 휴가 코드
+    )
+    REFERENCES cms_vKind ( -- 휴가 종류 
+      vCode -- 휴가 코드
+    );
+
+-- 휴가 신청
+ALTER TABLE cms_vApp
+  ADD CONSTRAINT FK_cms_worker_TO_cms_vApp -- 사원 -> 휴가 신청
+    FOREIGN KEY (
+      wNo -- 사원 번호
+    )
+    REFERENCES cms_worker ( -- 사원
+      wNo -- 사원 번호
+    );
+
+-- 댓글 정보
+ALTER TABLE cms_cmtInfo
+  ADD CONSTRAINT FK_cms_bList_TO_cms_cmtInfo -- 게시글 정보 -> 댓글 정보
+    FOREIGN KEY (
+      bNo -- 게시글번호
+    )
+    REFERENCES cms_bList ( -- 게시글 정보
+      bNo -- 게시글번호
+    );
+
+-- 댓글 정보
+ALTER TABLE cms_cmtInfo
+  ADD CONSTRAINT FK_cms_worker_TO_cms_cmtInfo -- 사원 -> 댓글 정보
+    FOREIGN KEY (
+      wNo -- 사원 번호
+    )
+    REFERENCES cms_worker ( -- 사원
+      wNo -- 사원 번호
+    );
+
+-- 일정
+ALTER TABLE cms_calendar
+  ADD CONSTRAINT FK_cms_worker_TO_cms_calendar -- 사원 -> 일정
+    FOREIGN KEY (
+      wNo -- 사원 번호
+    )
+    REFERENCES cms_worker ( -- 사원
+      wNo -- 사원 번호
+    );
+
+-- 대화
+ALTER TABLE cms_chat
+  ADD CONSTRAINT FK_cms_cGroup_TO_cms_chat -- 대화방참여자 -> 대화
+    FOREIGN KEY (
+      wNo,       -- 사원 번호
+      gChatCode  -- 대화방번호
+    )
+    REFERENCES cms_cGroup ( -- 대화방참여자
+      wNo,       -- 사원 번호
+      gChatCode  -- 대화방번호
+    );
+
+-- 대화방참여자
+ALTER TABLE cms_cGroup
+  ADD CONSTRAINT FK_cms_worker_TO_cms_cGroup -- 사원 -> 대화방참여자
+    FOREIGN KEY (
+      wNo -- 사원 번호
+    )
+    REFERENCES cms_worker ( -- 사원
+      wNo -- 사원 번호
+    );
+
+-- 대화방참여자
+ALTER TABLE cms_cGroup
+  ADD CONSTRAINT FK_cms_cRoom_TO_cms_cGroup -- 대화방 -> 대화방참여자
+    FOREIGN KEY (
+      gChatCode -- 대화방번호
+    )
+    REFERENCES cms_cRoom ( -- 대화방
+      gChatCode -- 대화방번호
+    );
+    
+    
+    
+-- 권한 예제데이터
+insert into cms_auth(aCode, name)
+values('0', '사원');
+
+insert into cms_auth(aCode, name)
+values('1', '인사');
+
+insert into cms_auth(aCode, name)
+values('9', '관리자');
+
+-- 회사정보 예제데이터
+insert into cms_cInfo(crNo, cName, map, postNo, addr, tel, fax)
+values('1248100998', '하이워크', '1', '1234', '서울특별시 서초구 신논현', '02-1111-1111', '02-3333-3333');
+
+-- 팀목록 예제데이터
+insert into cms_tList(tCode, crNo, tName)
+values('1', '1248100998', '개발팀');
+insert into cms_tList(tCode, crNo, tName)
+values('2', '1248100998', '인사팀');
+insert into cms_tList(tCode, crNo, tName)
+values('9', '1248100998', '관리팀');
+
+
+-- 사원 예제데이터
+insert into cms_worker(wNo, aCode, tCode, wName, email, wPhoto, rank, eType,
+regiDate, tel, gender, tAnnu, pwd, eSt)
+values(1, 0, '1', '김태희', 'aaa@test.com', '태희.jpg', '사장', '계약직',
+'2020-07-13', '01092628654', '2', '12', '1111', '0');
+
+insert into cms_worker(wNo, aCode, tCode, wName, email, wPhoto, rank, eType,
+regiDate, tel, gender, tAnnu, pwd, eSt)
+values(2, 1, '2', '홍길동', 'bbb@test.com', '길동.jpg', '사원', '계약직',
+'2020-07-13', '01011111111', '1', '13', '1111', '0');
+
+insert into cms_worker(wNo, aCode, tCode, wName, email, wPhoto, rank, eType,
+regiDate, tel, gender, tAnnu, pwd, eSt)
+values(3, 9, '9', '임꺽정', 'ccc@test.com', '꺽정.jpg', '대리', '정규직',
+'2020-07-13', '01022222222', '1', '12', '1111', '0');
+
+insert into cms_worker(wNo, aCode, tCode, wName, email, wPhoto, rank, eType,
+regiDate, tel, gender, tAnnu, pwd, eSt)
+values(4, 9, '9', '나퇴근', 'ddd@test.com', '퇴근.jpg', '대리', '정규직',
+'2020-07-13', '01033333333', '1', '12', '1111', '1');
+
+-- 일정 예제데이터
+insert into cms_calender(calCode, wNo, title, sdt, edt, content, place)
+values('1', 1, '회의', '2020-07-13', '2020-12-31', '회의하는날', '회사');
+
+insert into cms_calender(calCode, wNo, title, sdt, edt, content, place)
+values('2', 2, '회의2', '2020-07-13', '2020-12-31', '회의하는날2', '회사');
+
+insert into cms_calender(calCode, wNo, title, sdt, edt, content, place)
+values('3', 2, '발표', '2020-11-17', '2020-11-18', '발표하는날', '회사');
+
+-- 휴가 신청
+insert into cms_vApp(COL, vCode, wNo, adt, sdt, edt, used, rs, appr)
+values(1, 2)
+
+
