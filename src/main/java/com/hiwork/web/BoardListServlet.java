fa886d54e49ac354eb92961429ef55c42096603b
KimYuah <www0157@naver.com>
2020-11-24 19:18:21 +0900

    .

--- src/main/java/com/hiwork/web/BoardListServlet.java
+++ src/main/java/com/hiwork/web/BoardListServlet.java
@@ -2,6 +2,7 @@ package com.hiwork.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -24,8 +25,7 @@ public class BoardListServlet extends HttpServlet {
     BoardService boardService =
         (BoardService) ctx.getAttribute("boardService");
 
-
-    response.setContentType("text/plain;charset=UTF-8");
+    response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
 
     out.println("<!DOCTYPE html>");
@@ -33,27 +33,44 @@ public class BoardListServlet extends HttpServlet {
     out.println("<head><title>게시글목록</title></head>");
     out.println("<body>");
     try {
-      out.println("<h1>[게시물 목록]</h1>");
+      out.println("<h1>게시물 목록</h1>");
 
       List<Board> list = boardService.list();
       out.println("<table border='1'>");
-      out.println("<tr>");
-      out.println("번호, 제목, 작성자, 등록일, 조회수");
+      out.println("<tr>" // table row
+          + "<th>번호</th>" // table header
+          + "<th>제목</th>"
+          + "<th>작성자</th>"
+          + "<th>등록일</th>"
+          + "<th>조회수</th>"
+          + "</tr>");
 
       for (Board board : list) {
-        out.printf("%d, %s, %s, %s, %d\n",
+        out.printf("<tr>"
+            + "<td>%d</td>"
+            + "<td>%s</td>"
+            + "<td>%s</td>"
+            + "<td>%s</td>"
+            + "<td>%d</td>"
+            + "</tr>\n",
             board.getNo(),
             board.getTitle(),
             board.getWriter().getName(),
             board.getRegisteredDate(),
             board.getViewCount());
       }
+      out.println("</table>");
+
     } catch (Exception e) {
-      out.printf("작업 처리 중 오류 발생! - %s\n", e.getMessage());
-      e.printStackTrace();
+      out.printf("<p>작업 처리 중 오류 발생! - %s</p>\n", e.getMessage());
+
+      StringWriter errOut = new StringWriter();
+      e.printStackTrace(new PrintWriter(errOut));
+
+      out.printf("<pre>%s</pre>\n", errOut.toString());
     }
     out.println("</body>");
     out.println("</html>");
   }
-}
 
+}
