a4a831907f500dd7967bd1f0f64e768122645476
LeeYongMin01 <vbn0213@gmail.com>
2020-11-24 17:44:06 +0900

    프로젝트 공유

---
+++ src/main/java/com/hiwork/web/BoardListServlet.java
@@ -0,0 +1,58 @@
+package com.hiwork.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.hiwork.domain.Board;
+import com.hiwork.service.BoardService;
+
+@CommandAnno("/board/list")
+public class BoardListServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    ServletContext ctx = request.getServletContext();
+    BoardService boardService =
+        (BoardService) ctx.getAttribute("boardService");
+
+
+    response.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head><title>게시글목록</title></head>");
+    out.println("<body>");
+    try {
+      out.println("<h1>[게시물 목록]</h1>");
+
+      List<Board> list = boardService.list();
+      out.println("<table border='1'>");
+      out.println("<tr>");
+      out.println("번호, 제목, 작성자, 등록일, 조회수");
+
+      for (Board board : list) {
+        out.printf("%d, %s, %s, %s, %d\n",
+            board.getNo(),
+            board.getTitle(),
+            board.getWriter().getName(),
+            board.getRegisteredDate(),
+            board.getViewCount());
+      }
+    } catch (Exception e) {
+      out.printf("작업 처리 중 오류 발생! - %s\n", e.getMessage());
+      e.printStackTrace();
+    }
+    out.println("</body>");
+    out.println("</html>");
+  }
+}
+
