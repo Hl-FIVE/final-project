292ae78caf8a8a3783665d5d8ecdf10e5103659b
j-jieun <jsje02@gmail.com>
2020-11-26 19:11:11 +0900

    커밋

---
+++ src/main/java/com/hiwork/web/WorkerServlet.java
@@ -0,0 +1,9 @@
+package com.hiwork.web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/auth/list")
+public class WorkerServlet extends HttpServlet {
+
+}
